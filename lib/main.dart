695b7016f0e493672e8677867f513120d82a94e6
lickelon <jindoman0@naver.com>
2025-10-15 02:29:18 +0900

    Construct base app structure

--- lib/main.dart
+++ lib/main.dart
@@ -9,30 +9,78 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4, // ← Tab 개수와 동일해야 함
+      child: Scaffold(
         appBar: AppBar(
           title: Row(
-            spacing: 8,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [Icon(Icons.apps), Text("앱")],
-          ),
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-        ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [Icon(Icons.star), Icon(Icons.star)],
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Icon(Icons.phone),
-              Icon(Icons.message),
-              Icon(Icons.contact_page),
+              Row(
+                children: [
+                  IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+                  Text('Alchemist Hunter'),
+                ],
+              ),
+              TextButton(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [Text("Diamonds"), Icon(Icons.diamond)],
+                ),
+                onPressed: () {},
+              ),
             ],
           ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: const TabBar(
+            indicatorColor: Colors.transparent,
+            labelPadding: EdgeInsets.zero,
+            tabs: [
+              Tab(
+                child: Column(
+                  children: [Icon(Icons.person), Text('Characters')],
+                ),
+              ),
+              Tab(child: Column(children: [Icon(Icons.spa), Text('Weapons')])),
+              Tab(child: Column(children: [Icon(Icons.shield), Text('Armor')])),
+              Tab(child: Column(children: [Icon(Icons.pets), Text('Pets')])),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.grey,
+                      ),
+                      child: Center(child: Text('Character ${index + 1}')),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Center(child: Text('Page 2')),
+            Center(child: Text('Page 3')),
+            Center(child: Text('Page 4')),
+          ],
         ),
       ),
     );
